28fc1ab60d9d99d2ff68de16a4885e2ca59b1408
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-11-27 21:18:18 -0800

    Travel Discovery app setup

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+   var body: some View {
+      NavigationView {
+         
+         ScrollView {
+            DiscoverCategoriesView()
+         }.navigationTitle("Discovery")
+      }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
+}
+
+struct DiscoverCategoriesView: View {
+   
+   var body: some View {
+      ScrollView(.horizontal, showsIndicators: false) {
+         HStack(spacing: 8) {
+            
+            ForEach(0..<5, id: \.self) { num in
+               VStack {
+                  Spacer()
+                     .frame(width: 60, height: 60)
+                     .background(Color.gray)
+                     .cornerRadius(.infinity)
+                     .shadow(radius: 4, x: 0.0, y: 2)
+                  Text("Art")
+                     .font(.system(size: 12, weight: .semibold))
+               }
+            }
+         }.padding(.horizontal)
+      }
+   }
 }
